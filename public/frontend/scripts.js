bdf3778567713b06d66d702a1f33547a1b3ee241
TanVuong <tanvuong222@gmail.com>
2022-05-05 22:37:58 +0700

    Thêm file giỏ hàng

--- public/frontend/scripts.js
+++ public/frontend/scripts.js
@@ -1,8 +1,10 @@
 /* Toggle menu */
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("small-img");
 
 function menutoggle(){
     var MenuItems = document.getElementById("MenuItems");
-    
+
     MenuItems.style.maxHeight = "0px";
 
     if(MenuItems.classList.contains("active")){
@@ -20,3 +22,16 @@ function menutoggle(){
         // MenuItems.hide();
     }
 }
+    SmallImg[0].onclick = function(){
+    ProductImg.src = SmallImg[0].src;
+}
+    SmallImg[1].onclick = function(){
+    ProductImg.src = SmallImg[1].src;
+}
+    SmallImg[2].onclick = function(){
+    ProductImg.src = SmallImg[2].src;
+}
+    SmallImg[3].onclick = function(){
+    ProductImg.src = SmallImg[3].src;
+}
+
