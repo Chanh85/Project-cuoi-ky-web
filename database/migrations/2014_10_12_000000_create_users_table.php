7d99ecc10f990d7ddf727fa7f0e53082cc5f5d82
Chanh85 <chanh773er@yahoo.com>
2022-06-21 01:11:07 +0700

    phan quyen admin vs user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('gender');
+            $table->integer('role_id')->default(2);
             $table->rememberToken();
             $table->timestamps();
         });
