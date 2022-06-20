57da8207e390f432551420db54c33e9bf8439d97
TanVuong <tanvuong222@gmail.com>
2022-06-20 15:22:46 +0700

    Login, register, contact us, file public

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/frontend/scripts.js"></script>
-    <link rel="stylesheet" href="/frontend/style.css">
-
+    <script src="/frontend/js/scripts.js"></script>
+    <link rel="stylesheet" href="/frontend/css/style_contact.css">
+    <link rel="stylesheet" href="/frontend/css/style.css">
     <title>Contact-Us</title>
 </head>
 <body>
@@ -28,7 +28,7 @@
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                     @endif
-    
+
                     @if (Route::has('register'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -39,14 +39,14 @@
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
-    
+
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
-    
+
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
