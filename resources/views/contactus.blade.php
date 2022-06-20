f8aab187a666689a900c267cd552c31f58dea4d9
BlackWolf2002 <duynguyen11520@gmail.com>
2022-06-20 15:27:12 +0700

    aaa

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -28,7 +28,7 @@
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                     @endif
-    
+
                     @if (Route::has('register'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -39,14 +39,14 @@
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
-    
+
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
-    
+
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
@@ -78,7 +78,7 @@
 
             <div class="center">
                 <input type ="submit" value="Send Message">
-                <p id="success"></p>
+                <p id="success"></p>            
             </div>
         </form>
     </div>
